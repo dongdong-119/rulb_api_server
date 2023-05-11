1e5fb47d1658379f8614a253464de493291d6fb9
조유림 <whdbfla6@gmail.com>
2023-05-11 14:57:59 +0900

    모델 이식

--- main/controller/picture_controller.py
+++ main/controller/picture_controller.py
@@ -3,7 +3,10 @@ from flask_restx import Resource, Namespace
 from werkzeug.utils import secure_filename
 
 from main.service import picture_service
-
+from main.yolov5 import detect
+import cv2
+import os
+import shutil
 
 """
     [사진 관련 API] : /picture
@@ -31,29 +34,45 @@ class PicturePost(Resource):
 
         # 1. 데이터 검증
         image = picture_service.get_image(request)
-
-        image_name = secure_filename(str(image.filename))
-
-        # image.save(secure_filename(image_name))
+        RESULT = './main/yolov5/runs/detect/exp'
+        FILE = image.filename
+        IMG = FILE.split('.')[0]
+        
+        image.save(secure_filename(FILE))
+        img = cv2.imread(FILE)
+        height, width, _ = img.shape
 
         # 2. ai 모델에 넣어서 pred 받아옴
         # >> Return : pred(사각형 좌표, 가로 세로 길이)
+        detect.run(weights='best.pt', source='./', save_txt=True)
 
+        boxes,faceCnt = [],0
+        try:
+            with open(f'{RESULT}/labels/{IMG}.txt','r') as f:
+                while True:
+                    tmp = f.readline()
+                    if not tmp:
+                        break
+                    tmp = list(map(float,tmp.rstrip().split()))[1:]
+                    x, y, w, h = tmp
+                    x, w = x*width, w*width
+                    x -= w/2
+                    y, h = y*height, y*height
+                    y -= h/2
+                    boxes.append({'x':x, 'y':y, 'width':w, 'height':h})
+                    faceCnt += 1
+        except:
+            print('결과 반환 실패')
+            pass
 
-        # 3. DTO 형식으로 jsonify해서 반환
-        # >> Return : 2의 결과(dict) /
+        if os.path.exists(FILE):
+            os.remove(FILE)
+            print('이미지 삭제 완료')
+        if os.path.exists(RESULT):
+            shutil.rmtree(RESULT)
+            print('삭제완료')
 
-        result = dict()
-
-        # 얼굴 인식 완료
-        if result:
-            return jsonify(result)
-        # 얼굴 인식 불가능
-        else:
-            return {
-                'result': '결과',
-                'status': '성공'
-            }
+        return jsonify(boxes)
 
 
 # @Picture.route('/')
